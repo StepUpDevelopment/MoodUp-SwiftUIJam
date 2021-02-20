e19194b8958c8120925d6e4978c75d520e5d667f
Manuel Kunz <kunz.manuelk@gmail.com>
2021-02-20 23:38:33 +0100

    Add pie chart for stats view

---
+++ MoodUp/MoodUp/Features/Stats/View/PieChart.swift
@@ -0,0 +1,74 @@
+//
+//  PieChart.swift
+//  MoodUp
+//
+//  Created by Manuel Kunz on 20.02.21.
+//
+
+import Foundation
+import SwiftUI
+
+struct PieChart : View {
+	
+	var angles: [TmpAngle] = []
+	
+	init(values: [MoodTypeValue]) {
+		angles = startEndAngles(from: values)
+	}
+	
+	@ViewBuilder
+	var body: some View {
+		ZStack {
+			ForEach(angles, id: \.self) { angle in
+				Path { path in
+					path.move(to: CGPoint(x: 187, y: 187))
+					path.addArc(center: .init(x: 187, y: 187), radius: 150, startAngle: angle.start, endAngle: angle.range, clockwise: false)
+				}
+				.fill(angle.color)
+			}
+		}
+	}
+	private func startEndAngles(from values: [MoodTypeValue]) -> [TmpAngle] {
+				
+		let sum = values.reduce(0) { $0 + $1.value }
+
+		var tuples: [TmpAngle] = []
+		var currentStartAngle = 0.0
+		values.forEach { value in
+			let endAngle = ((360.0/sum) * value.value) + currentStartAngle
+			tuples.append(TmpAngle(start: Angle(degrees: currentStartAngle), range: Angle(degrees: endAngle),color: value.type.color))
+			currentStartAngle = endAngle
+		}
+
+		return tuples
+	}
+	
+	
+}
+
+struct MoodTypeValue {
+	var type: MoodType
+	var value: Double
+}
+struct TmpAngle : Hashable {
+	var start: Angle
+	var range: Angle
+	var color: Color
+	func hash(into hasher: inout Hasher) {
+		hasher.combine(start)
+		hasher.combine(range)
+	}
+}
+
+struct PieChart_Previews: PreviewProvider {
+	static var previews: some View {
+		PieChart(values: [
+			MoodTypeValue(type: .bad, value: 10),
+			MoodTypeValue(type: .awful, value: 5),
+			MoodTypeValue(type: .excellent, value: 25),
+			MoodTypeValue(type: .great, value: 25),
+			MoodTypeValue(type: .meh, value: 80)
+
+		])
+	}
+}
